cc79a0f00dedc47395f5bf3bae371eae4cb2381b
Robiul Islam Sumon <robiulislam984453@gmail.com>
2025-05-11 18:02:16 +0600

    our bookedParcel data get on client and create a get method here

--- server.js
+++ server.js
@@ -2,20 +2,19 @@ require('dotenv').config()
 const express = require('express')
 var cors = require('cors')
 const app = express()
+var jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000
 
 app.use(cors({
-    origin: [
-        "http://localhost:5173",
-    ],
-    credentials: true
+  origin: [
+    "http://localhost:5173",
+  ],
+  credentials: true
 }))
 app.use(express.json())
 
 
-
-// const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.f0yik.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.f0yik.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -29,29 +28,120 @@ const client = new MongoClient(uri, {
 
 
 async function run() {
-    const userCollection = client.db('parcelProDB').collection('users')
+  const userCollection = client.db('parcelProDB').collection('users')
+  const BookedParcelDB = client.db('parcelProDB').collection('bookedParcel')
   try {
-    
-    app.get('/', (req, res) => {
-        res.send('Hello World!')
+
+    // jwt related API
+    app.post('/jwt', (req, res) => {
+      const userEmail = req.body
+      const token = jwt.sign({
+        data: userEmail
+      }, `${process.env.SECRET_TOKEN}`, { expiresIn: '1h' });
+      res.send({ token })
     })
 
-    app.post('/users',async (req,res)=>{
-        const userData = req.body
-        const query = {email: userData.email}
-        const isExistUser = await userCollection.findOne(query)
-        if(isExistUser){
-          return res.send({message: "User already exist!", insertedId:null})
+    // Verify Token
+    const verifyToken = (req, res, next) => {
+      console.log("Request Interceptor token:",req?.headers?.authorization)
+      if (!req?.headers?.authorization) {
+        return res.status(401).send({ message: 'unAuthorization Access' })
+      }
+
+      const token = req.headers.authorization.split(' ')[1]
+      jwt.verify(token, `${process.env.SECRET_TOKEN}`, function (err, decoded) {
+        if(err){
+          return res.status(401).send({ message: 'unAuthorization Access' })
         }
-        const result = await userCollection.insertOne(userData)
-        res.send(result)
+        req.decoded = decoded
+        next()
+      });
+    }
+
+    // CRUD Operation Here...
+    app.get('/', (req, res) => {
+      res.send('Hello World!')
+    })
+
+    // Admin Check api
+    app.get('/users/admin/:email',async (req, res) => {
+        const email = req?.params?.email
+        // if(email !== req?.decoded?.email){
+        //   return res.status(403).send({ message: 'Forbidden Access' })
+        // }
+
+        const query = {email: email}
+        const user = await userCollection.findOne(query)
+        let isAdmin = false;
+
+        if(user){
+          isAdmin = user?.role == 'Admin'
+        }
+        res.send({isAdmin})
+    })
+
+    // deliveryMen Check api
+    app.get('/users/deliveryMen/:email',async (req, res) => {
+        const email = req?.params?.email
+        // if(email !== req?.decoded?.email){
+        //   return res.status(403).send({ message: 'Forbidden Access' })
+        // }
+
+        const query = {email: email}
+        const user = await userCollection.findOne(query)
+        let isDeliveryMen = false;
+
+        if(user){
+          isDeliveryMen = user?.role == 'DeliveryMen'
+        }
+        res.send({isDeliveryMen})
+    })
+
+    // user Check api
+    app.get('/users/user/:email',async (req, res) => {
+        const email = req?.params?.email
+        // if(email !== req?.decoded?.email){
+        //   return res.status(403).send({ message: 'Forbidden Access' })
+        // }
+
+        const query = {email: email}
+        const user = await userCollection.findOne(query)
+        let isUser= false;
+
+        if(user){
+          isUser = user?.role == 'User'
+        }
+        res.send({isUser})
+    })
+
+    app.get('/bookedParcel',async(req,res)=>{
+      const bookedData = req.body
+      const result = await BookedParcelDB.find(bookedData).toArray()
+      res.send(result)
+    })
+
+    app.post('/users', async (req, res) => {
+      const userData = req.body
+      const query = { email: userData.email }
+      const isExistUser = await userCollection.findOne(query)
+      if (isExistUser) {
+        return res.send({ message: "User already exist!", insertedId: null })
+      }
+      const result = await userCollection.insertOne(userData)
+      res.send(result)
+    })
+
+    app.post('/bookedParcel',async(req,res)=>{
+      const bookedData = req.body
+      const result = await BookedParcelDB.insertOne(bookedData)
+      res.send(result)
     })
 
     app.listen(port, () => {
-        console.log(`Example app listening on port ${port}`)
-      })
+      console.log(`Example app listening on port ${port}`)
+    })
 
-      
+
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
